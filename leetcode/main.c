e28b3cf80e68ad6eb29dcf53f158352b3f723323
anhkh04 <phamanhkhoa9803@gmail.com>
2025-05-04 20:23:46 +0700

    1. Two Sum

--- leetcode/main.c
+++ leetcode/main.c
@@ -1,1 +1,37 @@
-sdafsdf
+#include <stdio.h>
+#include <stdlib.h>
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize)
+{
+    int* res = (int*)malloc(2 * sizeof(int));
+
+    *returnSize = 2;
+    for (int i = 0; i < numsSize - 1; i++)
+    {
+        for (int j = i + 1; j < numsSize; j++)
+        {
+            if (nums[i] + nums[j] == target)
+            {
+                res[0] = i;
+                res[1] = j;
+
+                return res;
+            }
+        }
+    }
+
+    return 0;
+}
+
+int main()
+{
+    int nums[] = { 2, 7, 11, 15 };
+    int newsize; 
+    int* res = twoSum(nums, 4, 9, &newsize);
+
+    for (int i = 0; i < newsize; i++)
+    {
+        printf("%d ", res[i]);
+    }
+    return 0;
+}
