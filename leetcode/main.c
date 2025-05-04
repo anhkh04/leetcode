c6a8c1160532fb98c05a0ef8319bfcd8369e8537
anhkh04 <phamanhkhoa9803@gmail.com>
2025-05-04 20:34:38 +0700

    14. Longest Common Prefix

--- leetcode/main.c
+++ leetcode/main.c
@@ -1,78 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int romanToInt(char* s) {
-    int res = 0;
+char* longestCommonPrefix(char** strs, int strsSize) {
 
-    int i = 0;
-    while (s[i] != 0)
+    for (int j = 0; ; j++)
     {
-        switch (s[i])
+        for (int i = 0; i < strsSize - 1; i++)
         {
-        case 'I':
-            res += 1;
-            break;
-        case 'V':
-            if (i > 0 && s[i - 1] == 'I')
+            if (strs[i][j] != strs[i + 1][j])
             {
-                res += 3;
+                strs[0][j] = 0;
                 break;
             }
-            res += 5;
-            break;
-        case 'X':
-            if (i > 0 && s[i - 1] == 'I')
-            {
-                res += 8;
-                break;
-            }
-            res += 10;
-            break;
-        case 'L':
-            if (i > 0 && s[i - 1] == 'X')
-            {
-                res += 30;
-                break;
-            }
-            res += 50;
-            break;
-        case 'C':
-            if (i > 0 && s[i - 1] == 'X')
-            {
-                res += 80;
-                break;
-            }
-            res += 100;
-            break;
-        case 'D':
-            if (i > 0 && s[i - 1] == 'C')
-            {
-                res += 300;
-                break;
-            }
-            res += 500;
-            break;
-        case 'M':
-            if (i > 0 && s[i - 1] == 'C')
-            {
-                res += 800;
-                break;
-            }
-            res += 1000;
-            break;
-        default:
-            break;
         }
-        i++;
+
+        if (strs[0][j] == 0)
+        {
+            return strs[0];
+        }
     }
-
-    return res;
 }
-
 
 int main()
 {
-    printf("%d", romanToInt("LVIII"));
+    char* s[] = { "flower", "flow", "flight" };
 
+
+    char* res = longestCommonPrefix(s, 5);
     return 0;
 }
