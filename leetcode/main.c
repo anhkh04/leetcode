62ff4e930aaf33be934fc7cfe2a0b818a55ed7c7
anhkh04 <phamanhkhoa9803@gmail.com>
2025-05-05 15:10:21 +0700

    100. Same Tree

--- leetcode/main.c
+++ leetcode/main.c
@@ -3,45 +3,28 @@
 #include <string.h>
 #include <math.h>
 
-void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
 
-    int last;
-    if (m > 0) last = nums1[m - 1];
-
-    for (int i = 0, j = 0; j != n;)
+ struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+ };
+ 
+bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
+    if (p == NULL && q == NULL)
     {
-        if (nums1[i] <= nums2[j])
-        {
-            if (m == 0)
-            {
-                for (int k = 0; k < n; k++)
-                {
-                    nums1[k] = nums2[k];
-                }
-                return;
-            }
-            else if (nums1[i] == last)
-            {
-                for (int k = m + j; k < nums1Size; k++, j++)
-                {
-                    nums1[k] = nums2[j];
-                }
-                return;
-            }
-
-            i++;
-        }
-        else if (nums1[i] > nums2[j])
-        {
-            for (int k = m + j; k > i; k--)
-            {
-                nums1[k] = nums1[k - 1];
-            }
-            nums1[i] = nums2[j];
-            j++;
-        }
+        return true;
     }
-    return;
+    else if (p == NULL || q == NULL || p->val != q->val)
+    {
+        return false;
+    }
+
+    if (isSameTree(p->left, q->left) && isSameTree(p->right, q->right))
+    {
+        return true;
+    }
+    return false;
 }
 
 int main()
