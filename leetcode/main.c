8ab6b34ecc88d9930f5d55d765f3ee017d4224aa
anhkh04 <phamanhkhoa9803@gmail.com>
2025-05-05 15:11:02 +0700

    118. Pascal's Triangle

--- leetcode/main.c
+++ leetcode/main.c
@@ -3,28 +3,30 @@
 #include <string.h>
 #include <math.h>
 
+int** generate(int numRows, int* returnSize, int** returnColumnSizes)
+{
+	int** arr = malloc(sizeof(int*) * numRows);
+	*returnColumnSizes = malloc(sizeof(int) * numRows);
 
- struct TreeNode {
-    int val;
-    struct TreeNode *left;
-    struct TreeNode *right;
- };
- 
-bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
-    if (p == NULL && q == NULL)
-    {
-        return true;
-    }
-    else if (p == NULL || q == NULL || p->val != q->val)
-    {
-        return false;
-    }
+	for (int i = 0; i < numRows; i++)
+	{
+		(*returnColumnSizes)[i] = i + 1;
+		arr[i] = malloc(sizeof(int) * (i + 1));
+		for (int j = 0; j <= i; j++)
+		{
+			if (j == 0 || j == i)
+			{
+				arr[i][j] = 1;
+			}
+			else
+			{
+				arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+			}
+		}
+	}
 
-    if (isSameTree(p->left, q->left) && isSameTree(p->right, q->right))
-    {
-        return true;
-    }
-    return false;
+	*returnSize = numRows;
+	return arr;
 }
 
 int main()
