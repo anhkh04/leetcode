28d50056648b376e041bccdf021a29a93fb3399c
anhkh04 <phamanhkhoa9803@gmail.com>
2025-05-06 10:28:24 +0700

    171. Excel Sheet Column Number

--- leetcode/main.c
+++ leetcode/main.c
@@ -4,48 +4,19 @@
 #include <math.h>
 #include <stdint.h>
 
-int majorityElement(int* nums, int numsSize) {
+#include <string.h>
+#include <math.h>
 
-    int arr[10000][2] = { 0 };
-
-    int row = 0;
-    for (int i = 0; i < numsSize; i++)
+int titleToNumber(char* columnTitle)
+{
+    int strSize = strlen(columnTitle);
+    int res = 0;
+    for (int i = strSize - 1, j = 0; i >= 0; i--, j++)
     {
-        int check = 1;
-        int index;
-
-        for (int j = 0; j < row; j++)
-        {
-            if (nums[i] == arr[j][0])
-            {
-                check = 0;
-                index = j;
-                break;
-            }
-        }
-        if (check == 0)
-        {
-            arr[index][1]++;
-        }
-        else
-        {
-            arr[row][0] = nums[i];
-            arr[row++][1]++;
-        }
+        res += (columnTitle[i] - 64) * pow(26, j);
     }
 
-    int index = 0;
-    int max = arr[index][1];
-    for (int i = 0; i < row; i++)
-    {
-        if (arr[i][1] > max)
-        {
-            max = arr[i][1];
-            index = i;
-        }
-    }
-
-    return arr[index][0];
+    return res;
 }
 
 int main()
