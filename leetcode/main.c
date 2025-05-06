1689a7facb23f13033d31e9cb404d254647b4421
anhkh04 <phamanhkhoa9803@gmail.com>
2025-05-06 10:29:23 +0700

    191. Number of 1 Bits

--- leetcode/main.c
+++ leetcode/main.c
@@ -4,65 +4,15 @@
 #include <math.h>
 #include <stdint.h>
 
-typedef struct
-{
-	char bit0 : 1;
-	char bit1 : 1;
-	char bit2 : 1;
-	char bit3 : 1;
-	char bit4 : 1;
-	char bit5 : 1;
-	char bit6 : 1;
-	char bit7 : 1;
-} byte;
-
-typedef union
-{
-	uint32_t read;
-	byte bytes[4];
-} uint_t;
-
-uint32_t reverseBits(uint32_t n) {
-
-	uint_t x = { 0 };
-	x.read = n;
-
-	for (int i = 0, j = 3; i < 2; i++, j--)
+int hammingWeight(int n) {
+	int cnt = 0;
+	while (n > 0)
 	{
-		char tmp = x.bytes[i].bit0;
-		x.bytes[i].bit0 = x.bytes[j].bit7;
-		x.bytes[j].bit7 = tmp;
-
-		tmp = x.bytes[i].bit1;
-		x.bytes[i].bit1 = x.bytes[j].bit6;
-		x.bytes[j].bit6 = tmp;
-
-		tmp = x.bytes[i].bit2;
-		x.bytes[i].bit2 = x.bytes[j].bit5;
-		x.bytes[j].bit5 = tmp;
-
-		tmp = x.bytes[i].bit3;
-		x.bytes[i].bit3 = x.bytes[j].bit4;
-		x.bytes[j].bit4 = tmp;
-
-		tmp = x.bytes[i].bit4;
-		x.bytes[i].bit4 = x.bytes[j].bit3;
-		x.bytes[j].bit3 = tmp;
-
-		tmp = x.bytes[i].bit5;
-		x.bytes[i].bit5 = x.bytes[j].bit2;
-		x.bytes[j].bit2 = tmp;
-
-		tmp = x.bytes[i].bit6;
-		x.bytes[i].bit6 = x.bytes[j].bit1;
-		x.bytes[j].bit1 = tmp;
-
-		tmp = x.bytes[i].bit7;
-		x.bytes[i].bit7 = x.bytes[j].bit0;
-		x.bytes[j].bit0 = tmp;
+		if ((n & 1) == 1) cnt++;
+		n >>= 1;
 	}
 
-	return x.read;
+	return cnt;
 }
 
 int main()
